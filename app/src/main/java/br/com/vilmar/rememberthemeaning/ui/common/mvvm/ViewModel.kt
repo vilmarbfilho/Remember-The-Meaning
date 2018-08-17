8d75e6edcdb835e0332758738d887409f43edb03
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2018-08-17 15:51:44 -0300

    Initial Refactor of Home for Main

---
+++ app/src/main/java/br/com/vilmar/rememberthemeaning/ui/common/mvvm/ViewModel.kt
@@ -0,0 +1,9 @@
+package br.com.vilmar.rememberthemeaning.ui.common.mvvm
+
+interface ViewModel {
+
+    fun onAttach()
+
+    fun onDetach()
+
+}
---
+++ app/src/main/java/br/com/vilmar/rememberthemeaning/ui/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package br.com.vilmar.rememberthemeaning.ui.main
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.vilmar.rememberthemeaning.app.R
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
